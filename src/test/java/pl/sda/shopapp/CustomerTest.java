9c64830de96524547d3c4a46b7c4aa67579e1457
Lukasz Stepkowski <l.stepkow@gmail.com>
2020-06-06 10:24:13 +0200

    created test for Company class

--- src/test/java/pl/sda/shopapp/CustomerTest.java
+++ src/test/java/pl/sda/shopapp/CustomerTest.java
@@ -5,10 +5,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import pl.sda.shopapp.entity.Address;
+import pl.sda.shopapp.entity.Company;
 import pl.sda.shopapp.entity.Person;
+import pl.sda.shopapp.entity.VatNumber;
 
 import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 public class CustomerTest {
@@ -31,7 +35,28 @@ public class CustomerTest {
         var readPerson = em.find(Person.class, person.getId());
 
         //then
-        Assertions.assertEquals(readPerson, person);
-        Assertions.assertEquals(1, readPerson.getAddresses().size());
+        assertEquals(readPerson, person);
+        assertEquals(1, readPerson.getAddresses().size());
+    }
+
+    @Test
+    @Transactional
+    void testCreateCompany(){
+        //given
+        var company = new Company(new VatNumber("1234567890"), "Test S.A.");
+        var address = new Address("test", "test", "01-500", "PL");
+        company.addAddress(address);
+
+        //when
+        em.persist(company);
+        em.flush();
+        em.clear();
+        var readCompany = em.find(Company.class, company.getId());
+
+        //then
+        assertEquals(readCompany, company);
+        assertEquals(1, readCompany.getAddresses().size());
+        assertEquals(readCompany.getAddresses().get(0), address);
+
     }
 }
