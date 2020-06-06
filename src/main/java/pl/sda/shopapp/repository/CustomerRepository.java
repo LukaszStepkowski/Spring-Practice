d71b58580c7320de74eb021b4cc81f5f579f5194
Lukasz Stepkowski <l.stepkow@gmail.com>
2020-06-06 11:39:12 +0200

    added a findByName method to CustomerRepository and tested it

--- src/main/java/pl/sda/shopapp/repository/CustomerRepository.java
+++ src/main/java/pl/sda/shopapp/repository/CustomerRepository.java
@@ -3,7 +3,10 @@ package pl.sda.shopapp.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.sda.shopapp.entity.Customer;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface CustomerRepository extends JpaRepository<Customer, UUID> {
+
+    List<Customer> findByName(String name);
 }
--- src/test/java/pl/sda/shopapp/CustomerRepositoryTest.java
+++ src/test/java/pl/sda/shopapp/CustomerRepositoryTest.java
@@ -11,8 +11,7 @@ import pl.sda.shopapp.repository.CustomerRepository;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 public class CustomerRepositoryTest {
@@ -49,6 +48,21 @@ public class CustomerRepositoryTest {
         var sortedCustomers = repository.findAll(Sort.by(Sort.Direction.DESC, "name"));
         assertEquals(company, sortedCustomers.get(0));
         assertEquals(person, sortedCustomers.get(1));
+    }
 
+    @Test
+    void testFindByName(){
+        //given
+        var company = new Company(new VatNumber("1234567890"), "Test S.A.");
+        var companyAddress = new Address("test", "test", "01-500", "PL");
+        company.addAddress(companyAddress);
+
+        //when
+        repository.save(company);
+        var customers = repository.findByName("Test S.A.");
+
+        //then
+        assertFalse(customers.isEmpty());
+        assertEquals("Test S.A.", customers.get(0).getName());
     }
 }
